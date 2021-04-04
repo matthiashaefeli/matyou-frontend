00917051665ad1c7dd97075ecf977bc10793e9ba
matthiashaefeli <haefeli.matthias2gmail.com>
2021-04-04 17:39:15 -0500

    add image to cube, transition to explode cube

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,20 +1,32 @@
 import React from 'react';
-import './home.css';
+import './home.scss';
 import { useHistory } from 'react-router-dom';
 
 const Home = () => {
   const history = useHistory();
-  const handleClick = (route) => history.push(`/${route}`);
+
+  const handleClick = (event) => {
+    console.log(event.target.className)
+    console.log(event.target.dataset.val)
+    document.getElementsByClassName('front')[0].style.transform = 'translateZ(2000px) rotateZ(360deg)'
+    document.getElementsByClassName('back')[0].style.transform = 'translateZ(-2000px) rotateY(180deg)  rotateZ(360deg)'
+    document.getElementsByClassName('left')[0].style.transform = 'translateX(-2000px) rotateY(-90deg) rotateZ(360deg)'
+    document.getElementsByClassName('right')[0].style.transform = 'translateX(2000px) rotateY(90deg) rotateZ(360deg)'
+    document.getElementsByClassName('up')[0].style.transform = 'translateY(-2000px) rotateX(90deg) rotateZ(360deg)'
+    document.getElementsByClassName('down')[0].style.transform = 'translateY(2000px) rotateX(-90deg) rotateZ(360deg)'
+    const route = event.target.innerText.toLowerCase()
+    setTimeout(() => {  history.push(`/${route}`) }, 1000);
+  };
 
   return (
     <div className='container'>
     <div className='cube'>
-      <div className='side front' onClick={() => handleClick('notes')}>Notes</div>
-      <div className='side back' onClick={() => handleClick('challenges')}>Challenges</div>
-      <div className='side left' onClick={() => handleClick('books')}>Books</div>
-      <div className='side right' onClick={() => handleClick('about')}>About</div>
-      <div className='side up'>Coming soon</div>
-      <div className='side down' onClick={() => handleClick('blogs')}>Blog</div>
+      <div className='front' onClick={handleClick}>Notes</div>
+      <div className='back' onClick={handleClick}>Challenges</div>
+      <div className='left' onClick={handleClick}>Books</div>
+      <div className='right' onClick={handleClick}>About</div>
+      <div className='up'>Coming soon</div>
+      <div className='down' onClick={handleClick}>Blogs</div>
     </div>
   </div>
   );
