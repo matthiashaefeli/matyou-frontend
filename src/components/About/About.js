eeb6565d6280de9817f9f5879694325175ce857c
matthiashaefeli <haefeli.matthias2gmail.com>
2021-04-13 12:06:51 -0500

    cleanup warnings

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -20,15 +20,15 @@ class About extends Component {
           HAVE FUN!
         </div>
         <div className='aboutLinks'>
-          <a href='https://github.com/matthiashaefeli' target='_blank' rel="noopener noreferrer" >
+          <a href='https://github.com/matthiashaefeli' target='_blank' rel="noopener noreferrer">
             <FontAwesomeIcon icon={faGithub} />
           </a>
-          <a href='https://www.linkedin.com/in/matthias-haefeli/' target='_blank' rel="noopener noreferrer" >
+          <a href='https://www.linkedin.com/in/matthias-haefeli/' target='_blank' rel="noopener noreferrer">
             <FontAwesomeIcon icon={faLinkedin} />
           </a>
         </div>
         <div className='loginLink'>
-          <a href='https://warm-anchorage-02243.herokuapp.com/' target='_blank'>.</a>
+          <a href='https://warm-anchorage-02243.herokuapp.com/' target='_blank' rel="noopener noreferrer">.</a>
         </div>
       </article>
     )
