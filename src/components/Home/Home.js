1178468408c538af2936e458f8ad3cf94b9793be
matthiashaefeli <haefeli.matthias2gmail.com>
2021-04-24 14:36:11 -0500

    hide settings

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -87,7 +87,7 @@ class Home extends Component {
               <div className='cubeDiv' id='down' onClick={this.handleClick} data-style='translateY(2000px) rotateX(-90deg) rotateZ(360deg)'>Blogs</div>
             </div>
           </div>
-          <div>
+          <div className='settings'>
             <div className='backgroundButtons'>
               <p>Background</p>
               <div>
