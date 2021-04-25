ca856ae7ce5205e070187da93b0910436d1dc962
matthiashaefeli <haefeli.matthias2gmail.com>
2021-04-25 10:18:49 -0500

    add style random function

---
+++ src/components/StyleThree/Index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Index extends Component {
+  render() {
+    return (
+      <div>
+        <p>styleThree</p>
+      </div>
+    );
+  }
+}
+
+export default Index;
---
+++ src/components/StyleTwo/Index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Navbar from '../Navbar/Navbar';
+
+class Index extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar />
+      </div>
+    );
+  }
+}
+
+export default Index;
