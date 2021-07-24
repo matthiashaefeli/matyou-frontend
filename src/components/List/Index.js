c04f3ed1c756829f95c28048b481d82cc6607b3e
matthiashaefeli <haefeli.matthias2gmail.com>
2021-07-24 09:42:57 -0500

    bookmarks

--- src/components/List/Index.js
+++ src/components/List/Index.js
@@ -6,7 +6,7 @@ import Navbar from '../Navbar/Navbar';
 
 class Index extends Component {
   render() {
-    const titleText = "Here are all my links, like the bookmarks on the Internet Browser"
+    const titleText = "Here are all my links, like Bookmarks on the Web Browser"
 
     return (
       <article className='noteContainerHome'>
